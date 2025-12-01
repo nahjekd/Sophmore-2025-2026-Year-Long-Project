07c4f50080846ffa23acfe976716335102e0caed
Nahje <nahjekd209@gmail.com>
2025-12-01 08:36:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var navLinks = document.getElementById("navLinks");
 
+  // Functions to show/hide mobile menu
+  window.showMenu = function() {
+    navLinks.style.right = "0";
+  }
+
+  window.hideMenu = function() {
+    navLinks.style.right = "-200px";
+  }
+});
